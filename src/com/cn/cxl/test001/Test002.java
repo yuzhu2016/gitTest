7c6240e1723d6aa28bfd819b66a54b9423af4b6f
yuzhu2016 <yuzhu2015@gmail.com>
2016-09-11 22:03:27 +0900

    c

---
+++ src/com/cn/cxl/test001/Test002.java
@@ -0,0 +1,10 @@
+package com.cn.cxl.test001;
+
+public class Test002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("11111");   
+	}
+ 
+}
